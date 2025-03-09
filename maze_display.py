8c647beccfa1a4976524109e1555abedea2bda92
BagelDogM <bageldogm@gmail.com>
2025-03-09 22:36:01 +0000

    Display code cleanup

--- maze_display.py
+++ maze_display.py
@@ -1,24 +1,22 @@
 import pygame
 def generate_display_cells(rdicts, maze_grid):
+    """
+    Return an easier to display list of cells in [right_edge:bool, bottom_edge:bool] format for displaying
+    """
     display_cell_grid = []
 
     for x, column in enumerate(maze_grid):
         new_column = []
         for y, cell in enumerate(column):
-            # Handle row
             rindex = cell["row"]
-            if rindex:
-                right = rdicts["row"][rindex]["head"] == (x, y) # whether it is a head
-            else:
-                right = True
+            # If the cell doesn't exist, then there is a right line.
+            # Otherwise, the cell has a right line if it is the head of a row.
+            right = not rindex or rdicts["row"][rindex]["head"] == (x, y) # whether it is a head
 
-            # Handle column
             cindex = cell["col"]
-            #is_head = False
-            if cindex:
-                bottom = rdicts["col"][cindex]["head"] == (x, y) # whether it is a head
-            else:
-                bottom = True
+            # If the cell doesn't exist, then there is a bottom line.
+            # Otherwise, the cell has a bottom line if is the head of a column.
+            bottom = not cindex or rdicts["col"][cindex]["head"] == (x, y) # whether it is a head
 
             new_column.append([bottom, right])
 
@@ -30,12 +28,15 @@ def display_display_cell_grid(display_cell_grid, surface, width, height, stroke_
     # Draw big bounding box
     pygame.draw.rect(surface, (0, 0, 0), [0, 0, width, height], stroke_width)
 
-    # Draw maze cells
+    # Draw maze cells.
+    # Subtract stroke width so that cells do not overlap with the bounding box.
     cell_width = (width-stroke_width)/len(display_cell_grid)
     cell_height = (height-stroke_width)/len(display_cell_grid[0])
+
     for x, column in enumerate(display_cell_grid):
         for y, cell in enumerate(column):
             bottom, right = cell
+            # Draw lines appropriately
             if bottom:
                 pygame.draw.line(surface, (0, 0, 0), [x*cell_width, (y+1)*cell_height], [(x+1)*cell_width, (y+1)*cell_height], stroke_width)
             if right:
