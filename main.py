1c56d0bcf2345b96e3fac3cde4e6b4773daab624
BagelDogM <bageldogm@gmail.com>
2025-03-09 22:23:51 +0000

    Code cleanup

--- main.py
+++ main.py
@@ -7,30 +7,27 @@ from maze_display import display_display_cell_grid, generate_display_cells
 Python maze generator using weighted priority queue fetching and corridor-based weighting with referential updates avoiding
 expensive cascades along the actual grid matrix.
 """
-canvas_width, canvas_height = 1400, 800
-
-GENERATION_DELAY_S = 0
+# Maze generation settings
 TARGET_LENGTH = 5
-
 minimum = 50
 frac = 1/3
 POPULATION_FUNC = lambda n: math.ceil(n*frac) if n > minimum/frac else minimum
 
+# Graphics settings
 INTERMEDIATE_GRAPHICS = True
 strokew = 1
 
+# Size settings
+canvas_width, canvas_height = 1400, 800
 maze_width = int(canvas_width/10)
 maze_height = int(canvas_height/10)
 
-print(f'Running with {TARGET_LENGTH=} {maze_width=} {maze_height=}, cells={maze_width*maze_height}')
-
-rc_metronome = 0
-
+# The current highest used id of the rows and columns. Increments upwards with each new row and column created.
 current_rc_ids = {"row": 1, "col": 1}
 
 # The actual maze grid
-maze_grid = [[{"row":None, "col": None} for _ in range(maze_height)] for _ in range(maze_width)]
-maze_grid[0][0] = {"row": 1, "col": 1} # Initialise top-left square
+maze_grid = [[{"row":None, "col": None} for _ in range(maze_height)]for _ in range(maze_width)]
+maze_grid[0][0] = {"row": 1, "col": 1}  # Initialise top-left square
 
 # The dictionary of rows. format: rindex: row
 rc_dicts = {
@@ -106,62 +103,52 @@ def connect_rc_to_new_cell(id: int, head_or_tail: str, row_or_col: str):
     """
     if not rc_dicts[row_or_col][id][head_or_tail+"_alive"]: # Quick check for invalid connections
         raise ValueError(f"Tried to connect a head/tail that was dead: the {head_or_tail} of {row_or_col}-{id}")
+
     # -----------------------
     # Generate prerequisites
     # ------------------------
     inverse_rc = "row" if row_or_col == "col" else "col" # Used for collateral checks
-    inverse_ht = "head" if head_or_tail == "tail" else "tail"
+    inverse_ht = "head" if head_or_tail == "tail" else "tail" # head -> tail and tail -> head
     dict_var = rc_dicts[row_or_col]
     inverse_dict_var =  rc_dicts[inverse_rc] # Used for collateral checks
-    # Generate x/y shift offsets for new/ahead cells
-    shift = {
-        "head": (1, 2),
-        "tail": (-1, -2)
-    }
-    new_shift, ahead_shift = shift[head_or_tail]
 
-    # Generate real x/y shifts. Change x for rows and y for cols.
+    # Generate x/y shift offsets for new/ahead cells
+    # Change x for rows and y for cols.
+    shift_sign = -1 if head_or_tail == "tail" else 1
     real_xy_shifts = {
-        "row": (new_shift, 0, ahead_shift, 0),
-        "col": (0, new_shift, 0, ahead_shift)
+        "row": (shift_sign, 0, shift_sign*2, 0),
+        "col": (0, shift_sign, 0, shift_sign*2)
     }
     new_shift_x, new_shift_y, ahead_shift_x, ahead_shift_y = real_xy_shifts[row_or_col]
 
     # The new head/tail
-    new_cell_coords = (dict_var[id][head_or_tail][0]+new_shift_x,
-                       dict_var[id][head_or_tail][1]+new_shift_y)
+    new_cell_coords = (dict_var[id][head_or_tail][0] + new_shift_x,
+                       dict_var[id][head_or_tail][1] + new_shift_y)
+
+    # The cell in front which may cause the tail/head to be dead.
+    ahead_cell_coords = (dict_var[id][head_or_tail][0] + ahead_shift_x,
+                         dict_var[id][head_or_tail][1] + ahead_shift_y)
 
     # Generate new id values for the new cell
     if row_or_col == "row": new_cell = {"row": id, "col": initialise_new_rc(*new_cell_coords, "col")}
     else:                   new_cell = {"row": initialise_new_rc(*new_cell_coords, "row"), "col": id}
-
-    # Generate locations where to check if a head/tail has been killed because of us
-    collateral_checks = {
-        "row": ((0, 1), (0, -1)),
-        "col": ((1, 0), (-1, 0))
-    }
-
-    collateral_cells = collateral_checks[row_or_col]
 
     # There is now no need to differentiate between row/col cases so we
     # can run this part indiscriminately.
     # ----------
     # Update RC and Cell values (e.g. actually connect)
     # ----------
+    dict_var[id]["weight"] += 1 # Increase weight by 1
 
-    dict_var[id]["weight"] += 1
-    # The cell in front which may cause the tail/head to be dead.
-    # We use get_maze_cell to check whether it exists/is visited
-    ahead_cell_coords = [dict_var[id][head_or_tail][0]+ahead_shift_x,
-                               dict_var[id][head_or_tail][1]+ahead_shift_y]
-    ahead_cell = get_maze_cell(*ahead_cell_coords)
-
-
+    # Set the new head in the rc dictionaries and the new references for the cell in the maze
     dict_var[id][head_or_tail] = new_cell_coords
     maze_grid[new_cell_coords[0]][new_cell_coords[1]] = new_cell
 
+    # Check whether the cell ahead of the new cell is already connected.
+    # If so, our cell and that cell's heads/tails (one each) die.
+    ahead_cell = get_maze_cell(*ahead_cell_coords)
     if ahead_cell is None or cell_visited(ahead_cell):
-        # This cell is already connected (cell-visited/"r" is not None)
+        # This cell is already connected (cell-visited)
         # or we're at the edge of the board (ahead_cell is None)
         # therefore this head/tail is dead
         dict_var[id][head_or_tail+"_alive"] = False
@@ -171,13 +158,11 @@ def connect_rc_to_new_cell(id: int, head_or_tail: str, row_or_col: str):
         if ahead_cell is not None:
             dict_var[ahead_cell[row_or_col]][inverse_ht+"_alive"] = False
 
-            # STORED:
-            # The cell exists and is connected and
-            # This if statement checks whether the cell is a head of its row/col. Right most list is list of head
-            # and tail positions, and we check this against the cell's co-ordinates.
-            # (new_cell_coords[0] + x, new_cell_coords[1] + y)) in [(h := rc_dicts[inverse_rc][cell[inverse_rc]])["head"],
-            #                                                       h["tail"]]:
-            # affected_rc = cell[inverse_rc]
+    # Generate locations where to check if a head/tail has been killed because of us
+    collateral_cells = {
+        "row": ((0, 1), (0, -1)),
+        "col": ((1, 0), (-1, 0))
+    }[row_or_col]
 
     # Check if we killed any other heads/tails by connecting this new cell (collateral damage)
     for x, y in collateral_cells:
@@ -192,7 +177,7 @@ def connect_rc_to_new_cell(id: int, head_or_tail: str, row_or_col: str):
             inverse_dict_var[affected_rcindex][head_or_tail_affected+"_alive"] = False
 
 def step():
-    global rc_metronome, n
+    global rc_metronome, steps
     # Sort rows and columns by weight
     alive_cols = [(id, "col", item) for id, item in rc_dicts["col"].items() if item["head_alive"] or item["tail_alive"]]
     alive_rows = [(id, "row", item) for id, item in rc_dicts["row"].items() if item["head_alive"] or item["tail_alive"]]
@@ -213,31 +198,31 @@ def step():
 
     rc_metronome ^= 1
 
-    print(f'At step {(n:=n+1)}, samples={POPULATION_FUNC(len(sorted_population))}')
+    print(f'At step {(steps:=steps+1)}, samples={POPULATION_FUNC(len(sorted_population))}')
 
-
+# Initialise pygame
 pygame.init()
 
 pygame.display.set_caption('Maze Generator')
 surface = pygame.display.set_mode((canvas_width, canvas_height))
 
-running = True
+# Main loop
+print(f'Running with {TARGET_LENGTH=} {maze_width=} {maze_height=}, cells={maze_width*maze_height}')
 
-n = 0
+rc_metronome = 0
+steps = 0
+running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEBUTTONUP:
-            step()
 
     surface.fill(pygame.Color('#ffffff'))
 
-    if step() or INTERMEDIATE_GRAPHICS:
+    if step() or INTERMEDIATE_GRAPHICS: # Display if the generation has finished or we are displaying intermediate graphics
         display_display_cell_grid(generate_display_cells(rc_dicts, maze_grid),
                                   surface, surface.get_width(), surface.get_height(), strokew)
 
     pygame.display.update()
-    time.sleep(GENERATION_DELAY_S)
 
 while True: pass
